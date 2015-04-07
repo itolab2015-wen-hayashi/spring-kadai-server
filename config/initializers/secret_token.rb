b1485bd348345de7beb3c65d16fd29bb780ed115
TopGear <tglover.nuppy@gmail.com>
2015-04-07 12:14:57 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GuGame::Application.config.secret_key_base = '1a15b5211175595758d29101e23c1721e8d965c144208fa1c84c8215d635907bdeced1f52b9b2d31558aa98d7ae0c53f7098757c0f562c3761d25d97a58847a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GuGame::Application.config.session_store :cookie_store, key: '_GuGame_session'
