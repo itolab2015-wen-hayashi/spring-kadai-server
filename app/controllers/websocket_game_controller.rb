df9daf184714f8bf4d226136d229d6759aa453be
Masayuki Hayashi <masayuki@camel.itolab.nitech.ac.jp>
2015-04-14 16:40:29 +0900

    機能追加

--- app/controllers/websocket_game_controller.rb
+++ app/controllers/websocket_game_controller.rb
@@ -3,7 +3,11 @@
 #
 class WebsocketGameController < WebsocketRails::BaseController
 
+	# 最大ゲームスコア. このスコアをユーザが超えたらゲーム終了.
+	GAME_END_SCORE = 1000
+
 	# 初期化
+	#
 	def initialize_session
 		logger.debug("initializing WebsocketGameController")
 		controller_store[:event_count] = 0
@@ -15,7 +19,10 @@ class WebsocketGameController < WebsocketRails::BaseController
 	def client_connected
 		client_id = client_id()
 		logger.debug("client connected: #{client_id}")
-		controller_store[:clients][:client_id] = connection()
+		controller_store[:clients][:client_id] = {
+			:connection => connection(),
+			:score => 0
+		}
 	end
 
 	# クライアント切断時のイベントハンドラ
@@ -50,7 +57,7 @@ class WebsocketGameController < WebsocketRails::BaseController
 
 			# とりあえず勝者を決定
 			controller_store[:round][:winner] = client_id,
-			controller_store[:round][:elapsed_time] = received_message[:elapsed_time]
+			controller_store[:round][:min_elapsed_time] = received_message[:elapsed_time]
 
 			# --> 全員にブロードキャスト
 			broadcast_message(:winner_approval, received_message)
@@ -68,9 +75,9 @@ class WebsocketGameController < WebsocketRails::BaseController
 			controller_store[:clients_in_round].delete(client_id)
 
 			# 勝者を更新
-			if received_message[:elapsed_time] < controller_store[:round][:elapsed_time] then
+			if received_message[:elapsed_time] < controller_store[:round][:min_elapsed_time] then
 				controller_store[:round][:winner] = client_id,
-				controller_store[:round][:elapsed_time] = received_message[:elapsed_time]
+				controller_store[:round][:min_elapsed_time] = received_message[:elapsed_time]
 			end
 
 			# すべてのクライアントから結果を受信したかどうか
@@ -88,7 +95,8 @@ class WebsocketGameController < WebsocketRails::BaseController
 		controller_store[:round] = {
 			:state => "WAITING",
 			:winner => nil,
-			:elapsed_time => 0
+			:min_elapsed_time => 0,
+			:add_score => 100
 		}
 
 		# メッセージ送信
@@ -102,15 +110,36 @@ class WebsocketGameController < WebsocketRails::BaseController
 	# ラウンドを終えるメソッド
 	#
 	private
-	def close_round()
+	def close_round
 		winner = controller_store[:round][:winner]
 		controller_store[:round][:state] = "CLOSED"
 
+		# 勝者に加点
+		controller_store[:clients][winner][:score] += controller_store[:round][:add_score]
+
+		# メッセージ送信
+		message_to_send = {
+			:winner => winner,
+			:add_score => controller_store[:round][:add_score]
+		}
+
+		broadcast_message(:close_round, message_to_send)
+
+		# ゲーム終了の判断
+		if controller_store[:clients][winner][:score] > GAME_END_SCORE then
+			close_game(winner)
+		end
+	end
+
+	# ゲームを終えるメソッド
+	#  winner: 勝者
+	private
+	def close_game(winner)
 		# メッセージ送信
 		message_to_send = {
 			:winner => winner
 		}
 
-		broadcast_message(:close_round, message_to_send)
+		broadcast_message(:close_game, message_to_send)
 	end
 end
